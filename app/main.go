edc68299b3c5f4ce471a9be2394e3047b334c23e
Bartłomiej Lichtański <bartek2.71@gmail.com>
2023-02-15 17:34:26 +0100

    Use a closure to inject a dependency

--- app/main.go
+++ app/main.go
@@ -27,29 +27,30 @@ func main() {
 	}
 	defer db.Close()
 
-	trademarkRepository = infrastructure.NewTrademarkRepository(db)
-	handleRequests()
+	handleRequests(infrastructure.NewTrademarkRepository(db))
 }
 
-var trademarkRepository domain.TrademarkRepository
-
-func handleRequests() {
+func handleRequests(trademarkRepository domain.TrademarkRepository) {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/v1/trademarks/word/{name}", wordTrademark)
-	router.HandleFunc("/api/v1/trademarks/word/similar/{name}", similarWordTrademarks)
+	router.HandleFunc("/api/v1/trademarks/word/{name}", wordTrademark(trademarkRepository))
+	router.HandleFunc("/api/v1/trademarks/word/similar/{name}", similarWordTrademarks(trademarkRepository))
 	log.Fatal(http.ListenAndServe(":80", router))
 }
 
-func wordTrademark(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	trademarkName := vars["name"]
-	trademark, _ := trademarkRepository.GetByName(trademarkName)
-	json.NewEncoder(w).Encode(trademark)
+func wordTrademark(trademarkRepository domain.TrademarkRepository) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		trademarkName := vars["name"]
+		trademark, _ := trademarkRepository.GetByName(trademarkName)
+		json.NewEncoder(w).Encode(trademark)
+	}
 }
 
-func similarWordTrademarks(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	trademarkName := vars["name"]
-	trademarks, _ := trademarkRepository.GetSimilarByName(trademarkName)
-	json.NewEncoder(w).Encode(trademarks)
+func similarWordTrademarks(trademarkRepository domain.TrademarkRepository) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		trademarkName := vars["name"]
+		trademarks, _ := trademarkRepository.GetSimilarByName(trademarkName)
+		json.NewEncoder(w).Encode(trademarks)
+	}
 }
